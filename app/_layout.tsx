def4a01aab4af63b6f8f61cf3b4cd599a942dba9
mustafakaratas <mustafakaratas4179@gmail.com>
2025-07-09 16:46:29 +0300

    landing page navigation, login page design update, register page design update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -37,6 +37,7 @@ export default function RootLayout() {
           </>
         ) : (
           <>
+            <Stack.Screen name="landing" options={{ headerShown: false }} />
             <Stack.Screen name="login" options={{ headerShown: false }} />
             <Stack.Screen name="register" options={{ headerShown: false }} />
             <Stack.Screen
